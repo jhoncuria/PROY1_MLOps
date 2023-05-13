3e1a0873164e846f073f3511f340712baab83f9b
jhoncuria <ejcuri@gmail.com>
2023-05-13 08:08:15 -0500

    actualizando nuestros archivos main y tranformaciones

--- main.py
+++ main.py
@@ -1,89 +1,66 @@
 import pandas as pd
+import numpy as np
 from fastapi import FastAPI
 
-meses_esp = {
-    'January': 'enero',
-    'February': 'febrero',
-    'March': 'marzo',
-    'April': 'abril',
-    'May': 'mayo',
-    'June': 'junio',
-    'July': 'julio',
-    'August': 'agosto',
-    'September': 'septiembre',
-    'October': 'octubre',
-    'November': 'noviembre',
-    'December': 'diciembre'
-}
 
-dias_semana = {
-    'Monday': 'Lunes',
-    'Tuesday': 'Martes',
-    'Wednesday': 'Miércoles',
-    'Thursday': 'Jueves',
-    'Friday': 'Viernes',
-    'Saturday': 'Sábado',
-    'Sunday': 'Domingo'
-}
 
 app = FastAPI()
 
-df = pd.read_csv(r'data_movies_corr.csv')
-df['release_date'] = pd.to_datetime(df['release_date'], format='%Y-%m-%d')
-df['month_release'] = df['release_date'].apply(lambda x: meses_esp[x.strftime('%B')])
-df['day_of_week_release'] = df['release_date'].apply(lambda x: dias_semana[x.strftime('%A')])
+df = pd.read_csv("..//movies_clean1.csv")
+#--------------------------------------------------------------------------------------------------
+
 
 
 @app.get('/peliculas_mes/{mes}')
-def peliculas_mes(mes:str):
-    '''Se ingresa el mes y la funcion retorna la cantidad de peliculas que se estrenaron ese mes historicamente'''
-    df_mes = df[df['month_release'] == mes]
+def peliculas_mes(mes: str):
+    '''Se ingresa el mes y la función retorna la cantidad de películas que se estrenaron ese mes históricamente'''
+    df_mes = df[df['month'] == mes]
     cantidad = len(df_mes)
     return {'mes': mes, 'cantidad': cantidad}
 
 
+
+
 @app.get('/peliculas_dia/{dia}')
-def peliculas_dia(dia:str):
-    '''Se ingresa el dia y la funcion retorna la cantidad de peliculas que se estrebaron ese dia historicamente'''
-    df_dia = df[df['day_of_week_release'] == dia]
+def peliculas_dia(dia: str):
+    '''Se ingresa el día y la función retorna la cantidad de películas que se estrenaron ese día históricamente'''
+    df_dia = df[df['day'] == dia]
     cantidad = len(df_dia)
     return {'dia': dia, 'cantidad': cantidad}
 
 @app.get('/franquicia/{franquicia}')
-def franquicia(franquicia:str):
-    '''Se ingresa la franquicia, retornando la cantidad de peliculas, ganancia total y promedio'''
-    franquicia_df = df[df['belongs_to_collection'].apply(lambda x: x is not None and x['name'] == franquicia)]
-    cantidad = len(franquicia_df)
-    ganancia_total = franquicia_df['revenue'].sum()
-    ganancia_promedio = franquicia_df['revenue'].mean() if cantidad > 0 else 0
+def franquicia(franquicia: str):
+    '''Se ingresa la franquicia, retornando la cantidad de películas, ganancia total y promedio'''
+    df_franquicia = df[df['belongs_to_collection'] == franquicia]
+    cantidad = len(df_franquicia)
+    ganancia_total = df_franquicia['revenue'].sum()
+    ganancia_promedio = df_franquicia['revenue'].mean()
     return {'franquicia': franquicia, 'cantidad': cantidad, 'ganancia_total': ganancia_total, 'ganancia_promedio': ganancia_promedio}
 
 @app.get('/peliculas_pais/{pais}')
-def peliculas_pais(pais:str):
-    '''Ingresas el pais, retornando la cantidad de peliculas producidas en el mismo'''
-    pais_df = df[df['production_countries'].apply(lambda x: any(d['name'] == pais for d in x))]
-    cantidad = len(pais_df)
+def peliculas_pais(pais: str):
+    '''Ingresas el país, retornando la cantidad de películas producidas en el mismo'''
+    df_pais = df[df['production_countries'].apply(lambda x: pais in str(x))]
+    cantidad = len(df_pais)
     return {'pais': pais, 'cantidad': cantidad}
 
 @app.get('/productoras/{productora}')
-def productoras(productora:str):
-    '''Ingresas la productora, retornando la ganancia toal y la cantidad de peliculas que produjeron'''
-    productora_df = df[df['production_companies'].apply(lambda x: any(d['name'] == productora for d in x))]
-    ganancia_total = productora_df['revenue'].sum()
-    cantidad = productora_df['revenue'].count()
+def productoras(productora: str):
+    '''Ingresas la productora, retornando la ganancia total y la cantidad de películas que produjeron'''
+    df_productora = df[df['production_companies'].apply(lambda x: productora in str(x))]
+    cantidad = len(df_productora)
+    ganancia_total = df_productora['revenue'].sum()
     return {'productora': productora, 'ganancia_total': ganancia_total, 'cantidad': cantidad}
-@app.get('/retorno/{pelicula}')
-def retorno(pelicula:str):
-    '''Ingresas la pelicula, retornando la inversion, la ganancia, el retorno y el año en el que se lanzo'''
-    pelicula_df = df.loc[df['title'] == pelicula]
-    inversion = pelicula_df['budget'].values[0]
-    ganancia = pelicula_df['revenue'].values[0]
-    retorno = pelicula_df['return'].values[0]
-    anio = pelicula_df['release_date'].dt.year.values[0]
-    return {'pelicula':pelicula, 'inversion':inversion, 'ganacia':ganancia,'retorno':retorno, 'anio':anio}
 
-# ML
-@app.get('/recomendacion/{titulo}')
-def recomendacion(titulo:str):
-    '''Ingresas un nombre de pelicula y te recomienda las similares en una lista'''
-    return {'lista recomendada': 'respuesta'}
+
+
+@app.get("/retorno/{pelicula}")
+def retorno(pelicula: str):
+    '''Ingresas la película, retornando la inversión, la ganancia, el retorno y el año en el que se lanzó'''
+    df_filtrado = df[df['title'].apply(lambda x: pelicula.strip() == str(x.strip()))]
+    inversion = df_filtrado["budget"].sum()
+    ganancia = df_filtrado["revenue"].sum()
+    retorno = df_filtrado["return"].sum()
+    anio = pd.to_datetime(df_filtrado["year"]).dt.year
+
+    return {'pelicula': pelicula, 'inversion': str(inversion), 'ganancia': str(ganancia), 'retorno': str(retorno), 'año':str(anio.item())}
---
+++ scripts/transformaciones.py
@@ -0,0 +1,108 @@
+# Importacion de librerias
+import pandas as pd
+import numpy as np
+import ast
+
+#--------------------------------------------------------------------------------------------------
+
+# Carga de datos
+#ruta = os.path.join('..', 'datasets', 'movies_dataset.csv')
+#df = pd.read_csv(ruta)
+# df = pd.read_csv(r'C:\Users\sramr\OneDrive\Desktop\PI01_DTS10\datasets\movies_dataset.csv')
+
+df = pd.read_csv("..//datasets//movies_dataset.csv")
+
+#--------------------------------------------------------------------------------------------------
+
+# Algunos campos, como belongs_to_collection, production_companies y otros (ver diccionario de datos)
+# están anidados, esto es o bien tienen un diccionario o una lista como valores en cada fila, ¡deberán 
+# desanidarlos para poder y unirlos al dataset de nuevo hacer alguna de las consultas de la API! O bien 
+# buscar la manera de acceder a esos datos sin desanidarlos.
+
+#--------------------------------------------------------------------------------------------------
+def fetch_name(obj): 
+    if isinstance(obj, str) and '{' in obj:
+        L=[]
+        for i in ast.literal_eval(obj):
+            L.append(i['name']);
+        return L
+
+def fetch_name2(obj): 
+        if isinstance(obj, str) and '{' in obj:
+        # print(obj)
+            dic = ast.literal_eval(obj)
+            return dic['name']
+
+df['genres'] = df['genres'].apply(fetch_name)
+df['belongs_to_collection'] = df['belongs_to_collection'].apply(fetch_name2)
+df['production_companies']  = df['production_companies'].apply(fetch_name)
+df['production_countries']  = df['production_countries'].apply(fetch_name)
+df['spoken_languages'] = df['spoken_languages'].apply(fetch_name)
+
+
+
+# Los valores nulos de los campos revenue, budget deben ser rellenados por el número 0.
+df = fillna_revenue_and_budget(df)
+
+df['revenue'] = df['revenue'].fillna(0)
+df['budget'] = df['budget'].fillna(0)
+
+#--------------------------------------------------------------------------------------------------
+
+# Los valores nulos del campo release date deben eliminarse.
+df = dropna_release_date(df)
+
+df.dropna(subset=['release_date'], inplace=True)
+
+# Convertir la columna release_date en formato datetime
+df['release_date'] = pd.to_datetime(df["release_date"], format='%Y-%m-%d', errors = 'coerce')
+df ['release_date'].dropna(inplace=True)
+mascara = df[df["release_date"].isnull()]
+df = df.drop(labels= mascara.index, axis=0)
+# Crear las columnas separadas para año, mes y día:
+df['release_year'] = df['release_date'].dt.year
+df['release_month'] = df['release_date'].dt.month
+df['release_day'] = df['release_date'].dt.day
+
+# Establecer la configuración regional en español
+locale.setlocale(locale.LC_TIME, 'es_ES.UTF-8')
+df["year"] = df['release_year']
+df["month"] = df['release_date'].dt.strftime('%B').apply(lambda x: x.capitalize() if type(x) != float else x)
+df["day"] = df['release_date'].dt.strftime('%A').apply(lambda x: x.capitalize() if type(x) != float else x)
+
+
+
+#--------------------------------------------------------------------------------------------------
+
+# Crear la columna con el retorno de inversión, llamada return con los campos revenue y budget,
+# dividiendo estas dos últimas revenue / budget, cuando no hay datos disponibles para calcularlo, 
+# deberá tomar el valor 0.
+df = add_inversion_column(df)
+
+# Limpiar y convertir a numérico las columnas revenue y budget
+
+df['revenue'] = pd.to_numeric(df['revenue'], errors='coerce').fillna(0)
+df['budget'] = pd.to_numeric(df['budget'], errors='coerce').fillna(0)
+
+# Calcular el return
+
+df['return'] = np.where(df['budget'] > 0, df['revenue'] / df['budget'], 0)
+df['return'] = df['return'].apply(lambda x: round(x, 2))
+
+
+
+#--------------------------------------------------------------------------------------------------
+
+# Eliminar las columnas que no serán utilizadas, video,imdb_id,adult,original_title,vote_count,
+# poster_path y homepage.
+df = drop_useless_columns(df)
+
+# Eliminacion de columnas
+df = df.drop(['video', 'imdb_id', 'adult', 'original_title', 'vote_count', 'poster_path', 'homepage', 'release_year', 'release_month', 'release_day',], axis=1)
+
+
+#--------------------------------------------------------------------------------------------------
+
+
+
+df.to_csv('movies_clean.csv', index=False, encoding='utf-8')
